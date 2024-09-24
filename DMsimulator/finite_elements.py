0d690d3214a11ba0e1578accd7ceb43342b4ae7d
Menessao <menessinimattoe@gmail.com>
2024-09-24 17:28:49 +0200

    Mesh function integrated in main

--- DMsimulator/finite_elements.py
+++ DMsimulator/finite_elements.py
@@ -5,150 +5,88 @@ import matplotlib.pyplot as plt
 from rotate_coordinates import rotate_by_60deg as rot60
 from rotate_coordinates import cw_rotate
 
-##########
-SIN60 = np.sin(np.pi/3.)
-COS60 = np.cos(np.pi/3.)
-
 def sum_n(n):
     return int(n*(n+1)/2)
 
-ul_triangle = np.zeros([3,2])
-ul_triangle[1,:] = np.array([2*COS60, 0.])
-ul_triangle[2,:] = np.array([0.5, SIN60])
+# Useful variables
+SIN60 = np.sin(np.pi/3.)
+COS60 = np.cos(np.pi/3.)
 
-# # Structured mesh
-# points_per_side = 10
-# plist = np.zeros([sum_n(points_per_side+1)+3,2])
 
-# plist[0:3,:] = ul_triangle
-# dx = 1./points_per_side
-# for k in np.arange(1,points_per_side+1):
-#     y = np.linspace(0.,SIN60*k*dx,k+1)
-#     x = k*dx - COS60/SIN60 * y
-#     plist[3+sum_n(k):3+sum_n(k+1),0] = x
-#     plist[3+sum_n(k):3+sum_n(k+1),1] = y
+def segment_act_coordinates(act_pitch, capsens_radius, hex_side_len):
+    acts_per_side = int((hex_side_len-2*capsens_radius)/act_pitch)
+    n_acts_tri = sum_n(acts_per_side)
+    act_coords = np.zeros([n_acts_tri*6+1,2])
+    dx = (1.-2*capsens_radius/hex_side_len)/acts_per_side
     
-# # Irregular mesh
-# Nel = 200
-# mu_x = COS60
-# # mu_y = SIN60
-# sigma_x = mu_x/2.4
-# # sigma_y = mu_y/3
-# yvec = np.random.random(Nel)
-# xvec = mu_x + sigma_x * np.random.randn(Nel)
-
-# xvec[xvec < 0.] = 1. - xvec[xvec < 0.]
-# xvec[xvec > 1.] = xvec[xvec > 1.] - 1.
-
-# xvec = np.sort(xvec)
-
-# y_start_max = xvec[xvec <= 0.5]*SIN60/COS60
-# y_end_max = (1.-xvec[xvec > 0.5])*SIN60/COS60
-
-# y_max = np.concatenate((y_start_max,y_end_max))
-# yvec[yvec > y_max] = y_max[yvec > y_max]
+    for k in range(acts_per_side):
+        y = np.linspace(SIN60*k*dx,0.,k+1)
+        x = (k+1)*dx - COS60/SIN60 * y
+        n = k+1
+        p = np.zeros([6*n,2])
+        p[0:n,0] = x
+        p[0:n,1] = y
+        p[n:2*n,:] = rot60(p[0:n,:].T)
+        p[2*n:3*n,:] = rot60(p[n:2*n,:].T)
+        p[3*n:,:] = cw_rotate(p[0:3*n,:].T,np.array([np.pi]))
+        act_coords[1+sum_n(k)*6:1+sum_n(k+1)*6,:] = p
+        
+    return act_coords
 
 
-# plist = np.zeros([len(yvec)+3,2])
-# plist[0:3,:] = ul_triangle
-# plist[3:,0] = xvec
-# plist[3:,1] = yvec
-
-
-# # Semi-structured mesh
-# points_per_side = 12
-# plist = np.zeros([sum_n(points_per_side+2),2])
-# ang = np.pi/3.
-
-# plist[0:3,:] = ul_triangle
-# dx = 1./points_per_side
-# ctr = 3
-# for k in np.arange(points_per_side):
-#     p = np.zeros([k+2,2])
-#     p[0,:] = [k*dx,0]
-#     p[-1,:] = [k*dx*COS60,k*dx*SIN60]
-#     angle = np.random.uniform(ang/(k+5),ang*(k+5)/(k+6),k) #np.random.randn(k)*sig + mu
-#     angle = np.sort(angle)
-#     p[1:-1,:] = cw_rotate(p[0,:],-angle)*SIN60
-#     # print(3+sum_n(k)+k+2-len(plist))
-#     plist[ctr:ctr+len(p),:] = p
-#     ctr += len(p)
+def semi_structured_point_cloud(points_per_side):
+    # Upper left triangle of the hexagon
+    ul_triangle = np.zeros([3,2])
+    ul_triangle[1,:] = np.array([2*COS60, 0.])
+    ul_triangle[2,:] = np.array([0.5, SIN60])
     
-# y = np.linspace(0.,SIN60,k+1)
-# x = 1. - COS60/SIN60 * y
-# plist[ctr:,0] = x
-# plist[ctr:,1] = y
+    plist = np.zeros([sum_n(points_per_side+1)+3,2])
 
-# Structured mesh + noise
-points_per_side = 24
-plist = np.zeros([sum_n(points_per_side+1)+3,2])
+    # Structured mesh + noise
+    plist[0:3,:] = ul_triangle
+    dx = 1./points_per_side 
+    sig = dx/7.5
+    for k in np.arange(1,points_per_side+1):
+        y = np.linspace(0.,SIN60*k*dx,k+1)
+        x = k*dx - COS60/SIN60 * y
+        if k < points_per_side:
+            y[1:-1] = y[1:-1] + np.random.randn(k-1)*sig
+            x[1:-1] = x[1:-1] + np.random.randn(k-1)*sig
+        plist[3+sum_n(k):3+sum_n(k+1),0] = x
+        plist[3+sum_n(k):3+sum_n(k+1),1] = y
 
-plist[0:3,:] = ul_triangle
-dx = 1./points_per_side 
-sig = dx/7.5
-for k in np.arange(1,points_per_side+1):
-    y = np.linspace(0.,SIN60*k*dx,k+1)
-    x = k*dx - COS60/SIN60 * y
-    if k < points_per_side:
-        y[1:-1] = y[1:-1] + np.random.randn(k-1)*sig
-        x[1:-1] = x[1:-1] + np.random.randn(k-1)*sig
-    plist[3+sum_n(k):3+sum_n(k+1),0] = x
-    plist[3+sum_n(k):3+sum_n(k+1),1] = y
+    points = np.zeros([len(plist)*6,2])
+    points[0:len(plist),:] = plist
 
-points = np.zeros([len(plist)*6,2])
-points[0:len(plist),:] = plist
+    for i in range(5):
+        points[(i+1)*len(plist):(i+2)*len(plist),:] = rot60(points[i*len(plist):(i+1)*len(plist),:])
 
-for i in range(5):
-    points[(i+1)*len(plist):(i+2)*len(plist),:] = rot60(points[i*len(plist):(i+1)*len(plist),:])
+    return points
 
-hex_side_len = 2.
-points = hex_side_len*points
 
-# Actuation points
-act_pitch = 0.4
-capsens_radius = 0.02
+def mesh_from_act_coordinates(points_per_side,act_coords,capsens_radius,hex_side_len):
+    point_cloud = semi_structured_point_cloud(points_per_side)
+    
+    n_acts = len(act_coords)
+    flat_act_coords = np.tile(act_coords,5).flatten()
+    up_down_left_right = np.array([[0,0],[0,1],[0,-1],[-1,0],
+                                   [1,0]]).flatten()
+    UDLR = np.tile(up_down_left_right,n_acts)
+    capsens_points = flat_act_coords + UDLR*capsens_radius/hex_side_len
+    capsens_points = np.reshape(capsens_points,[n_acts*5,2])
+    
+    points = np.concatenate((point_cloud,capsens_points))
+    
+    return points
 
-act_per_side = int((hex_side_len-2*capsens_radius)/act_pitch)
-n_acts_tri = sum_n(points_per_side)
-act_coords = np.zeros([n_acts_tri*6+1,2])
-dx = (hex_side_len-2*capsens_radius)/act_per_side
+def distribute_local_to_global(local_coords, hex_side_len, hex_center):
+    
+    global_coords = local_coords * hex_side_len + hex_center
+    
+    return global_coords
+    
+   
 
-act_size = capsens_radius*500 * np.ones(len(act_coords))
 
-# for k in range(act_per_side):
-#     y = np.linspace(0.,SIN60*k*dx,k+1)
-#     x = (k+1)*dx - COS60/SIN60 * y
-#     act_coords[1+sum_n(k):1+sum_n(k+1),0] = x
-#     act_coords[1+sum_n(k):1+sum_n(k+1),1] = y
-
-# for i in range(5):
-#     act_coords[1+(i+1)*n_acts_tri:1+(i+2)*n_acts_tri,:] = rot60(act_coords[1+i*n_acts_tri:1+(i+1)*n_acts_tri,:])
-
-for k in np.arange(0,act_per_side):
-    y = np.linspace(SIN60*k*dx,0.,k+1)
-    x = (k+1)*dx - COS60/SIN60 * y
-    n = k+1
-    p = np.zeros([6*n,2])
-    p[0:n,0] = x
-    p[0:n,1] = y
-    p[n:2*n,:] = rot60(p[0:n,:].T)
-    p[2*n:3*n,:] = rot60(p[n:2*n,:].T)
-    p[3*n:,:] = cw_rotate(p[0:3*n,:].T,np.array([np.pi]))
-
-    act_coords[1+sum_n(k)*6:1+sum_n(k+1)*6,:] = p
-
-tria = Delaunay(points) #,incremental=True
-plt.figure()
-plt.grid('on')
-plt.axis('equal')
-plt.triplot(points[:,0], points[:,1], tria.simplices,c='black')
-plt.scatter(act_coords[:,0], act_coords[:,1],s=act_size,c='red')
-plt.show()
-
-# tria.add_points(np.array([[0.222,0.234],[0.5,0.717],[0.333,0.125],[0.6,0.1222]]),restart=True)
-# plt.figure()
-# plt.grid('on')
-# plt.axis('equal')
-# plt.triplot(points[:,0], points[:,1], tria.simplices)
 
 
--- DMsimulator/main.py
+++ DMsimulator/main.py
@@ -1,15 +1,20 @@
-# import numpy as np
+import numpy as np
 import matplotlib.pyplot as plt
 from geometry import Hexagons
 # from astropy.io import fits
 # from scipy.sparse import csr_matrix 
 # from zernike_polynomials import computeZernike as czern
 
+# Mesh testing
+from finite_elements import segment_act_coordinates
+from finite_elements import mesh_from_act_coordinates
+from scipy.spatial import Delaunay
+
 config_tn = '20240920'
 hexes = Hexagons(config_tn)
 
 # Show the segments mask
-mask = hexes.full_mask
+mask = hexes.global_mask
 plt.figure()
 plt.imshow(mask,origin='lower',cmap='gray')
 
@@ -21,17 +26,35 @@ plt.figure()
 # plt.grid('on')
 plt.imshow(img,origin='lower',cmap='hot')
 
-# # Draw hexes and inscribed circle
-# hexes.draw_hex_outline()
+# Draw hexes and inscribed circle
+hexes.draw_hex_outline()
 
-# # Test Zernike modes
-# astig1 = czern(5,hexes.hex_mask)
-# astig2 = czern(6,hexes.hex_mask)
-# coma1 = czern(7,hexes.hex_mask)
-# coma2 = czern(8,hexes.hex_mask)
-# tref1 = czern(9,hexes.hex_mask)
-# tref2 = czern(10,hexes.hex_mask)
-# spher = czern(11,hexes.hex_mask)
+
+# Testing mesh
+hex_side_len = hexes.hex_side_len
+act_pitch = 0.3
+capsens_radius = 0.02
+points_per_side = 9 # very low for visualization purposes 
+
+local_act_coords = segment_act_coordinates(act_pitch, capsens_radius, hex_side_len)
+points = mesh_from_act_coordinates(points_per_side,local_act_coords, capsens_radius, hex_side_len)
+
+act_size = capsens_radius * 250 * np.ones(len(local_act_coords)) # plotting only
+
+plt.figure()
+plt.grid('on')
+plt.axis('equal')
+
+for center in hexes.hex_centers:
+    # print(center)
+    coords = points*hex_side_len + center
+    global_act_coords = local_act_coords*hex_side_len + center
+    tria = Delaunay(coords) #,incremental=True
+    plt.triplot(coords[:,0], coords[:,1], tria.simplices,c='black')
+    plt.scatter(global_act_coords[:,0], global_act_coords[:,1],s=act_size,c='red')
+
+plt.xlim([-2,2])
+plt.ylim([-2,2])
 
 
 
