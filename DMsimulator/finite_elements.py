cc50d7c1bcc1c0bab1d8989b8295fe597ddd3fa4
Menessao <menessinimattoe@gmail.com>
2024-09-24 11:00:40 +0200

    Working mesh geneartion code

--- DMsimulator/finite_elements.py
+++ DMsimulator/finite_elements.py
@@ -3,6 +3,7 @@ from scipy.spatial import Delaunay
 import matplotlib.pyplot as plt
 
 from rotate_coordinates import rotate_by_60deg as rot60
+from rotate_coordinates import cw_rotate
 
 ##########
 SIN60 = np.sin(np.pi/3.)
@@ -18,18 +19,18 @@ ul_triangle[1,:] = np.array([2*COS60, 0.])
 ul_triangle[2,:] = np.array([0.5, SIN60])
 
 # # Structured mesh
-points_per_side = 10
-plist = np.zeros([sum_n(points_per_side+1)+3,2])
+# points_per_side = 10
+# plist = np.zeros([sum_n(points_per_side+1)+3,2])
 
-plist[0:3,:] = ul_triangle
-dx = 1./points_per_side
-for k in np.arange(1,points_per_side+1):
-    y = np.linspace(0.,SIN60*k*dx,k+1)
-    x = k*dx - COS60/SIN60 * y
-    plist[3+sum_n(k):3+sum_n(k+1),0] = x
-    plist[3+sum_n(k):3+sum_n(k+1),1] = y
+# plist[0:3,:] = ul_triangle
+# dx = 1./points_per_side
+# for k in np.arange(1,points_per_side+1):
+#     y = np.linspace(0.,SIN60*k*dx,k+1)
+#     x = k*dx - COS60/SIN60 * y
+#     plist[3+sum_n(k):3+sum_n(k+1),0] = x
+#     plist[3+sum_n(k):3+sum_n(k+1),1] = y
     
-# Irregular mesh
+# # Irregular mesh
 # Nel = 200
 # mu_x = COS60
 # # mu_y = SIN60
@@ -56,6 +57,45 @@ for k in np.arange(1,points_per_side+1):
 # plist[3:,1] = yvec
 
 
+# # Semi-structured mesh
+# points_per_side = 12
+# plist = np.zeros([sum_n(points_per_side+2),2])
+# ang = np.pi/3.
+
+# plist[0:3,:] = ul_triangle
+# dx = 1./points_per_side
+# ctr = 3
+# for k in np.arange(points_per_side):
+#     p = np.zeros([k+2,2])
+#     p[0,:] = [k*dx,0]
+#     p[-1,:] = [k*dx*COS60,k*dx*SIN60]
+#     angle = np.random.uniform(ang/(k+5),ang*(k+5)/(k+6),k) #np.random.randn(k)*sig + mu
+#     angle = np.sort(angle)
+#     p[1:-1,:] = cw_rotate(p[0,:],-angle)*SIN60
+#     # print(3+sum_n(k)+k+2-len(plist))
+#     plist[ctr:ctr+len(p),:] = p
+#     ctr += len(p)
+    
+# y = np.linspace(0.,SIN60,k+1)
+# x = 1. - COS60/SIN60 * y
+# plist[ctr:,0] = x
+# plist[ctr:,1] = y
+
+# Structured mesh + noise
+points_per_side = 16
+plist = np.zeros([sum_n(points_per_side+1)*2+3,2])
+
+plist[0:3,:] = ul_triangle
+dx = 1./points_per_side 
+sig = dx/8.
+for k in np.arange(1,points_per_side+1):
+    y = np.linspace(0.,SIN60*k*dx,k+1)
+    x = k*dx - COS60/SIN60 * y
+    if k < points_per_side:
+        y[1:-1] = y[1:-1] + np.random.randn(k-1)*sig
+        x[1:-1] = x[1:-1] + np.random.randn(k-1)*sig
+    plist[3+sum_n(k):3+sum_n(k+1),0] = x
+    plist[3+sum_n(k):3+sum_n(k+1),1] = y
 
 points = np.zeros([len(plist)*6,2])
 points[0:len(plist),:] = plist
@@ -70,7 +110,7 @@ plt.figure()
 plt.grid('on')
 plt.axis('equal')
 plt.triplot(points[:,0], points[:,1], tria.simplices)
-plt.plot(ul_triangle[:,0], ul_triangle[:,1], 'o')
+plt.plot(plist[:,0], plist[:,1], 'o')
 plt.show()
 
 # tria.add_points(np.array([[0.222,0.234],[0.5,0.717],[0.333,0.125],[0.6,0.1222]]),restart=True)
@@ -78,3 +118,5 @@ plt.show()
 # plt.grid('on')
 # plt.axis('equal')
 # plt.triplot(points[:,0], points[:,1], tria.simplices)
+
+
