1a127cb9159d6152d56ca92ec3115275bb35b54d
Menessao <menessinimattoe@gmail.com>
2024-09-24 10:59:28 +0200

    Updated to work with multiple angles

--- DMsimulator/rotate_coordinates.py
+++ DMsimulator/rotate_coordinates.py
@@ -1,22 +1,29 @@
 import numpy as np
 
-def cw_rotate(vec, angle):
-    c = np.cos(angle)
-    s = np.sin(angle)
-    rot_mat = [[c,s],[-s,c]]
+def cw_rotate(vec, angles):
     
-    if np.shape(vec)[0] > 2:
-        aux_vec = rot_mat @ vec.transpose()
-        rot_vec = aux_vec.transpose()
-    else:
-        rot_vec = rot_mat @ vec
+    n_pts = int(np.size(vec)/2)
+    res_vec = np.zeros([n_pts*len(angles),2])
     
-    return rot_vec
+    for k, angle in enumerate(angles):
+        c = np.cos(angle)
+        s = np.sin(angle)
+        rot_mat = [[c,s],[-s,c]]
+        
+        if n_pts > 2:
+            aux_vec = rot_mat @ vec.transpose()
+            rot_vec = aux_vec.transpose()
+        else:
+            rot_vec = rot_mat @ vec
+            
+        res_vec[k*n_pts:(k+1)*n_pts,:] = rot_vec
+    
+    return res_vec
 
 
 def rotate_by_60deg(vec):
     # Wrapper to cw_rotate()
-    cw_angle = np.pi/3.
-    rot_vec = cw_rotate(vec, cw_angle)
+    angles = np.array([np.pi/3.])
+    rot_vec = cw_rotate(vec, angles)
     
     return rot_vec
