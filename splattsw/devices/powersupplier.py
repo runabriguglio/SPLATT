377697807fb2614cf0c3834acc8f59b953425cbf
Menessao <menessinimatteo@gmail.com>
2024-10-31 12:56:05 +0100

    Tried and tested powersupplier.py

--- splattsw/devices/powersupplier.py
+++ splattsw/devices/powersupplier.py
@@ -1,32 +1,36 @@
 import SPLATT.splattsw.devices.devices_scipi as scpi
 
-print("Warning!! Load your device with function ''update_device(deviceName)''")
-
-global name
-
-def update_device(deviceName):
-    global name
-    name = deviceName
-
+name = 'Rigol_PowerSupplier'
 default_state = 'splatt.RSF'
 
+# USE THE FOLLOWING IF MORE THAN 1 POWER SUPPLIERS ARE AVAILABLE
+# global name
+# print("Warning!! Load your device with function ''update_device(deviceName)''")
+# def update_device(deviceName):
+#     global name
+#     name = deviceName
+
 def reset():
-    print("WARNING! Following commmands might be corrupted")
     rp_s = scpi.scpi(name)
     rp_s.rst()
     rp_s.close()
 
 def switch_on(ch):
     rp_s = scpi.scpi(name)
-    rp_s.tx_txt(':OUTP CH'+str(ch)+' ON')
+    rp_s.tx_txt(':OUTP CH'+str(ch)+',ON')
     rp_s.close()
 
 def switch_off(ch):
     rp_s = scpi.scpi(name)
-    rp_s.tx_txt(':OUTP CH'+str(ch)+' OFF')
+    rp_s.tx_txt(':OUTP CH'+str(ch)+',OFF')
     rp_s.close()
 
-def load_saved_state(state_name):
+def load_saved_state(state_name = default_state):
     rp_s = scpi.scpi(name)
     rp_s.tx_txt(':MEM:LOAD '+ state_name)
     rp_s.close()
+
+# def save_current_state(state_name):
+#     rp_s = scpi.scpi(name)
+#     rp_s.tx_txt(':MEM:STOR '+ state_name + '.RSF')
+#     rp_s.close()
