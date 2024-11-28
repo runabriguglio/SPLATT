77df68d48eab46ad1c7c00d4d8ab8b72cd013843
Menessao <menessinimattoe@gmail.com>
2024-11-28 09:14:16 +0100

    Simplified segment class

--- DMsimulator/mirror_segment.py
+++ DMsimulator/mirror_segment.py
@@ -12,32 +12,6 @@ class Segment(DM):
         self.center = center_coordinates
         self.mask = mask
         self.valid_ids = valid_ids
-    
-    
-    def update_act_coords(self, new_act_coords):
-        """
-        Updates the actuator coordinates to new_act_coordinates 
-        (resetting their position to zero) 
-        
-
-        Parameters
-        ----------
-        new_act_coords : ndarray([n_acts,2])
-            Vector containing the new actuator coordinates.
-
-        Returns
-        -------
-        None.
-
-        """
-        
-        if np.abs(len(new_act_coords)-len(self.act_coords)):
-            raise NotImplementedError('Change in actuator number not implemented')
-        
-        # Actuator data
-        self.act_coords = new_act_coords
-        
-        # Update IFF and R ...
         
     
     
