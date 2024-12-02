c9a875e45660bbb87f04cc7463afadab6be57a1c
Menessao <menessinimattoe@gmail.com>
2024-12-02 09:15:38 +0100

    Added CapSens matrix

--- DMsimulator/matrix_calculator.py
+++ DMsimulator/matrix_calculator.py
@@ -26,12 +26,71 @@ def matmul(mat, vec):
         
     return res
 
+def define_capsens_matrix(mask, pix_scale, act_coords, r_in, r_out, capsens_coords = None):
+    """
+    Determine the pixel CapSens matrix to estimate the gap measured by
+    the capacitive sensors for a give shape as meas_gap = CSMAT @ shape
+
+    Parameters
+    ----------
+    mask : ndarray(bool)
+        The mask representing the mirror on which to operate.
+    pix_scale : float
+        The number of pixels per meter.
+    act_coords : ndarray(float) [Nacts,2]
+        The local actuator coordinates (in meters).
+    r_in : float
+        The CapSens inner radius (in meters).
+    r_out : float
+        The CapSens outer radius (in meters).
+    capsens_coords : ndarray(float) [Nacts,2], optional
+        The local actuator coordinates (in meters).
+        Useful if there is an eccentricity between 
+        CapSens and actuators. The default is act_coords.
+
+    Returns
+    -------
+    CSMAT : ndarray(float) [Nacts,Npixels]
+        The obtained CapSens pixel matrix, normalized by the
+        pixel area (i.e. number of pixels per CapSens)
+
+    """
+    
+    if capsens_coords is None:
+        capsens_coords = act_coords
+        
+    X,Y = np.shape(mask)
+    d = lambda i,j: np.sqrt(i**2+j**2)
+
+    # Rounding to int is less accurate but gives 
+    # more consistent results in terms of pixel area
+    pix_act_coords = (act_coords*pix_scale + np.array([Y,X])/2).astype(int)
+    pix_capsens_coords = (capsens_coords*pix_scale + np.array([Y,X])/2).astype(int)
+    
+    pix_in = int(r_in*pix_scale)
+    pix_out = int(r_out*pix_scale)
+    
+    CSMAT = np.zeros([len(act_coords),np.sum(1-mask)])
+    
+    for k, pix_act_coord in enumerate(pix_act_coords):
+        act_x, act_y = pix_act_coord
+        sens_x, sens_y = pix_capsens_coords[k,:]
+        sensor = np.fromfunction(lambda i,j: (d(i-act_y,j-act_x) >= pix_in) * (d(i-sens_y,j-sens_x) < pix_out), [X,Y])
+        # img = np.ma.masked_array(sensor,mask), plt.figure(), plt.imshow(img, origin = 'lower')
+        
+        masked_data = sensor[~mask]
+        pix_area = np.sum(masked_data)
+        # print(pix_area) # debug
+        CSMAT[k,:] = masked_data/pix_area
+    
+    return CSMAT
+
 
 def compute_reconstructor(M):
     return np.linalg.pinv(M)
 
 
-def simulate_influence_functions(act_coords, local_mask, pix_scale):
+def simulate_influence_functions(act_coords, local_mask, pix_scale, amp = 1.0):
     """ Simulate the influence functions by 
     imposing 'perfect' zonal commands """
     
@@ -51,7 +110,7 @@ def simulate_influence_functions(act_coords, local_mask, pix_scale):
 
     for k in range(n_acts):
         act_data = np.zeros(n_acts)
-        act_data[k] = 1
+        act_data[k] = amp
         tps = ThinPlateSpline(alpha=0.0)
         tps.fit(act_pix_coords, act_data)
         flat_img = tps.transform(pix_coords)
