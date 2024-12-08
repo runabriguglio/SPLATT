6ca05bfe74b7cf6ff8a1737b4686907f9a7f466e
Menessao <menessinimattoe@gmail.com>
2024-12-08 18:06:40 +0100

    Minor engine updates

--- matlabengine-server.py
+++ matlabengine-server.py
@@ -4,7 +4,7 @@ import numpy as np
 
 @Pyro4.expose
 class RemoteMatlab(object):
-    def start_engine(self):
+    def start_engine(self): 
         self.eng = matlab.engine.start_matlab()
         self.eng.desktop(nargout=0)
         self.eng.cd(r'../../SPLATT_SW/Matlab_2024/Matlab/Scripts')
@@ -12,8 +12,12 @@ class RemoteMatlab(object):
     def send_command(self,command):
         self.eng.eval(str(command)+';', nargout=0)
 
-    def get_data(self,command_to_read_data):
-        data = np.array(self.eng.eval(str(command_to_read_data)+';',nargout=1))
+    def get_data(self, command_to_read_data, n_args_out = 1):
+        output = self.eng.eval(str(command_to_read_data)+';',
+                                      nargout=n_args_out)
+        data = []
+        for k in range(n_args_out):
+            data.append(np.array(output[k]))
         return data
 
     def stop_engine(self):
