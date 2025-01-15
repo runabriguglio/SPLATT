688d2e63c50c2b104451ef93826a5726d417df81
runabriguglio <runa.briguglio@inaf.it>
2025-01-15 17:01:53 +0100

    it's working!

--- splattsw/test_receive.py
+++ splattsw/test_receive.py
@@ -4,12 +4,12 @@ import time
 ip = "192.168.0.101"
 port = 5555
 delimiter = '\r\n'
-rigol = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+RDP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 def open_read_ch1():
     try:
         print("Connetto")
-        RDP = rigol.connect((ip,port))
+        RDP.connect((ip,port))
         RDP.settimeout(2)
         print("Accendo il canale 1...")
         RDP.send((":OUTP CH1,ON" + delimiter).encode('utf-8'))
@@ -23,8 +23,45 @@ def open_read_ch1():
         RDP.send((":SYST:ERR?" + delimiter).encode('utf-8'))
         error_response = RDP.recv(4096).decode('utf-8').strip()
         print(f"Errore: {error_response}")
-    finally:
         RDP.close()
+    except socket.error as e:
+        print(f"SOck >> connection to {ip}:{port} failed: {e}")
+
+def open_read_close_read_ch1():
+    try:
+        print("Connetto")
+        RDP.connect((ip,port))
+        RDP.settimeout(2)
+        print("Accendo il canale 1...")
+        RDP.send((":OUTP CH1,ON" + delimiter).encode('utf-8'))
+        time.sleep(0.2)
+        print("Leggo lo stato del canale 1...")
+        RDP.send((":OUTP? CH1" + delimiter).encode('utf-8'))
+        time.sleep(0.2)
+        response = RDP.recv(4096).decode('utf-8').strip()
+        print(f"Stato canale 1: {response}")
+        print("Controllo errori...")
+        RDP.send((":SYST:ERR?" + delimiter).encode('utf-8'))
+        error_response = RDP.recv(4096).decode('utf-8').strip()
+        print(f"Errore: {error_response}")
+        print("\nAspetto 5 secondi...")
+        time.sleep(5)
+        print("\nSpegnimento canale 1")
+        RDP.send((":OUTP CH1,OFF" + delimiter).encode('utf-8'))
+        time.sleep(0.2)
+        print("Leggo lo stato del canale 1...")
+        RDP.send((":OUTP? CH1" + delimiter).encode('utf-8'))
+        time.sleep(0.2)
+        response = RDP.recv(4096).decode('utf-8').strip()
+        print(f"Stato canale 1: {response}")
+        print("Controllo errori...")
+        RDP.send((":SYST:ERR?" + delimiter).encode('utf-8'))
+        error_response = RDP.recv(4096).decode('utf-8').strip()
+        print(f"Errore: {error_response}")
+        RDP.close()
+    except socket.error as e:
+        print(f"SOck >> connection to {ip}:{port} failed: {e}")
+
 
 def read(cs=4096):
     """Receive text string and return it after removing the delimiter."""
