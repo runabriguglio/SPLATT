c22929c5420fcaccb632eed8a48b90d58efa764c
Menessao <menessinimatteo@gmail.com>
2025-03-10 10:26:44 +0100

    updated matlab engine class

--- splattsw/devices/matlab_engine_class.py
+++ splattsw/devices/matlab_engine_class.py
@@ -6,14 +6,14 @@ import numpy as np
 class MatlabEngine(object):
     def start_engine(self):
         self.eng = matlab.engine.start_matlab()
-        self.eng.desktop(nargout=0)
+        #self.eng.desktop(nargout=0)
         self.eng.cd(r'SPLATT_SW/Matlab_2024/Matlab/Scripts')
 
     def send_command(self,command):
         self.eng.eval(str(command)+';', nargout=0)
 
-    def get_data(self, command_to_read_data:str, n_args_out:int = 1, is_numeric:bool = True):
-        data = self.eng.eval(str(command_to_read_data),nargout=n_args_out)
+    def get_data(self, command_to_read_data:str, is_numeric:bool = True):
+        data = self.eng.eval(command_to_read_data,nargout=1)
         if is_numeric:
             data = np.array(data)
         return data
