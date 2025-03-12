70d6427ef4022e2527c0c4e83d0215152dfa024c
runabriguglio <runa.briguglio@inaf.it>
2025-03-12 10:07:51 +0100

    used pandas dataset in analysis

--- splattsw/test_analyse_acc_trig.py
+++ splattsw/test_analyse_acc_trig.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import splattsw.acceleration_analysis as anal
+import pandas as pd
 
 def acc_trig(wdflist, f_thr = 200):
 
@@ -42,7 +43,7 @@ def acc_trig(wdflist, f_thr = 200):
         # Find signal start
         ids = (np.minimum(np.sort(min6ids),np.sort(max6ids))).astype(int)
 
-        sig_len = np.arange(-100,1200,dtype=int)
+        sig_len = np.arange(0,1200,dtype=int)
         sig1 = acc[ids[2]+sig_len]
         sig2 = acc[ids[3]+sig_len]
 
@@ -52,7 +53,7 @@ def acc_trig(wdflist, f_thr = 200):
         mean_sig_spe.append((spe1+spe2)/2)
 
         # Find accelerometer signal
-        acc_len = np.arange(-10,500,dtype=int)
+        acc_len = np.arange(0,500,dtype=int)
         acc1 = acc[ids[0]+acc_len]
         acc2 = acc[ids[1]+acc_len]
         acc3 = acc[ids[4]+acc_len]
@@ -103,87 +104,97 @@ def acc_trig(wdflist, f_thr = 200):
     return max_sig_spe, f_max_sig, max_acc_spe, f_max_acc
 
 # Gap1, Vac2
-wdflist12 = ['OBB-Vibration_2025-02-18T17-03-39-102.wdd',
+wdflist = ['OBB-Vibration_2025-02-18T17-03-39-102.wdd',
  'OBB-Vibration_2025-02-18T17-04-35-988.wdd',
  'OBB-Vibration_2025-02-18T17-05-32-785.wdd',
  'OBB-Vibration_2025-02-18T17-06-29-635.wdd',
  'OBB-Vibration_2025-02-18T17-07-26-502.wdd',
- 'OBB-Vibration_2025-02-18T17-08-23-331.wdd']
-sig12, f_sig, acc12, f_acc = acc_trig(wdflist12)
+ 'OBB-Vibration_2025-02-18T17-08-23-331.wdd',
 
 # Gap2, Vac2
-wdflist22 = ['OBB-Vibration_2025-02-18T17-09-26-405.wdd',
+'OBB-Vibration_2025-02-18T17-09-26-405.wdd',
 'OBB-Vibration_2025-02-18T17-10-23-179.wdd',
 'OBB-Vibration_2025-02-18T17-11-19-966.wdd',
 'OBB-Vibration_2025-02-18T17-12-16-822.wdd',
 'OBB-Vibration_2025-02-18T17-13-13-576.wdd',
-'OBB-Vibration_2025-02-18T17-14-10-387.wdd']
-sig22, f_sig, acc22, f_acc = acc_trig(wdflist22)
+'OBB-Vibration_2025-02-18T17-14-10-387.wdd',
 
 # Gap3, Vac2
-wdflist32 = ['OBB-Vibration_2025-02-18T17-15-13-463.wdd',
+'OBB-Vibration_2025-02-18T17-15-13-463.wdd',
 'OBB-Vibration_2025-02-18T17-16-10-278.wdd',
 'OBB-Vibration_2025-02-18T17-17-07-141.wdd',
 'OBB-Vibration_2025-02-18T17-18-03-958.wdd',
 'OBB-Vibration_2025-02-18T17-19-00-840.wdd',
-'OBB-Vibration_2025-02-18T17-19-57-762.wdd']
-sig32, f_sig, acc32, f_acc = acc_trig(wdflist32)
-
+'OBB-Vibration_2025-02-18T17-19-57-762.wdd',
 
 
 # Gap1, Vac1
-wdflist11 = ['OBB-Vibration_2025-02-19T16-44-25-595.wdd',
+'OBB-Vibration_2025-02-19T16-44-25-595.wdd',
 'OBB-Vibration_2025-02-19T16-45-22-171.wdd',
 'OBB-Vibration_2025-02-19T16-46-17-195.wdd',
 'OBB-Vibration_2025-02-19T16-47-12-137.wdd',
 'OBB-Vibration_2025-02-19T16-48-07-156.wdd',
-'OBB-Vibration_2025-02-19T16-49-02-115.wdd']
-sig11, f_sig, acc11, f_acc = acc_trig(wdflist11)
+'OBB-Vibration_2025-02-19T16-49-02-115.wdd',
 
 # Gap2, Vac1
-wdflist21 = ['OBB-Vibration_2025-02-19T16-50-03-197.wdd',
+'OBB-Vibration_2025-02-19T16-50-03-197.wdd',
 'OBB-Vibration_2025-02-19T16-50-58-170.wdd',
 'OBB-Vibration_2025-02-19T16-51-53-062.wdd',
 'OBB-Vibration_2025-02-19T16-52-47-997.wdd',
 'OBB-Vibration_2025-02-19T16-53-43-069.wdd',
-'OBB-Vibration_2025-02-19T16-54-38-121.wdd']
-sig21, f_sig, acc21, f_acc = acc_trig(wdflist21)
+'OBB-Vibration_2025-02-19T16-54-38-121.wdd',
 
 # Gap3, Vac1
-wdflist31 = ['OBB-Vibration_2025-02-19T16-55-39-260.wdd',
+'OBB-Vibration_2025-02-19T16-55-39-260.wdd',
 'OBB-Vibration_2025-02-19T16-56-34-318.wdd',
 'OBB-Vibration_2025-02-19T16-57-29-240.wdd',
 'OBB-Vibration_2025-02-19T16-58-24-146.wdd',
 'OBB-Vibration_2025-02-19T16-59-19-186.wdd',
-'OBB-Vibration_2025-02-19T17-00-14-279.wdd']
-sig31, f_sig, acc31, f_acc = acc_trig(wdflist31)
-
-
-
+'OBB-Vibration_2025-02-19T17-00-14-279.wdd',
 
 # Gap1, Vac0
-wdflist10 = ['OBB-Vibration_2025-02-19T17-12-30-930.wdd',
+'OBB-Vibration_2025-02-19T17-12-30-930.wdd',
 'OBB-Vibration_2025-02-19T17-13-28-856.wdd',
 'OBB-Vibration_2025-02-19T17-14-23-680.wdd',
 'OBB-Vibration_2025-02-19T17-15-18-422.wdd',
 'OBB-Vibration_2025-02-19T17-16-13-135.wdd',
-'OBB-Vibration_2025-02-19T17-17-08-012.wdd']
-sig10, f_sig, acc10, f_acc = acc_trig(wdflist10)
+'OBB-Vibration_2025-02-19T17-17-08-012.wdd',
 
 # Gap2, Vac0
-wdflist20 = ['OBB-Vibration_2025-02-19T17-18-09-177.wdd',
+'OBB-Vibration_2025-02-19T17-18-09-177.wdd',
 'OBB-Vibration_2025-02-19T17-19-04-012.wdd',
 'OBB-Vibration_2025-02-19T17-19-58-780.wdd',
 'OBB-Vibration_2025-02-19T17-20-53-573.wdd',
 'OBB-Vibration_2025-02-19T17-21-48-374.wdd',
-'OBB-Vibration_2025-02-19T17-22-43-204.wdd']
-sig20, f_sig, acc20, f_acc = acc_trig(wdflist20)
+'OBB-Vibration_2025-02-19T17-22-43-204.wdd',
 
 # Gap3, Vac0
-wdflist30 = ['OBB-Vibration_2025-02-19T17-23-44-112.wdd',
+'OBB-Vibration_2025-02-19T17-23-44-112.wdd',
 'OBB-Vibration_2025-02-19T17-24-38-761.wdd',
 'OBB-Vibration_2025-02-19T17-25-33-692.wdd',
 'OBB-Vibration_2025-02-19T17-26-28-370.wdd',
 'OBB-Vibration_2025-02-19T17-27-23-218.wdd',
 'OBB-Vibration_2025-02-19T17-28-17-934.wdd']
-sig30, f_sig, acc30, f_acc = acc_trig(wdflist30)
+
+gap_vec = np.tile(np.repeat([1,2,3],6),3)
+vac_vec = np.repeat([2,1,0],18)
+mode_vec = np.tile([1,2,3],18)
+
+gap_list = []
+gap_list += ("Gap%s" %g for g in gap_vec)
+
+vac_list = []
+vac_list += ("Vac%s" %v for v in vac_vec)
+
+mode_list = []
+mode_list += ("Mode%s" %m for m in mode_vec)
+
+
+dataset = {
+    'gaps' : gap_list,
+    'vacuum' : vac_list,
+    'modes' : mode_list,
+    'wdf_tns' : wdflist
+}
+
+pdset = pd.DataFrame(dataset)
