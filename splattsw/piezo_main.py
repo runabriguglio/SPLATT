a3183499128bae7f8107bd8353f5db56e0f64986
runabriguglio <runa.briguglio@inaf.it>
2026-03-02 12:42:51 +0100

    Piezo circle fit

--- splattsw/piezo_main.py
+++ splattsw/piezo_main.py
@@ -78,7 +78,7 @@ def connect_to_camera():
 
 
 ##################### IM ########################
-def best_circle_fit(image, thr:float=0.5):
+def best_circle_fit(image, thr:float=0.5, show:bool=False):
     arr = np.asarray(image, dtype=np.float64)
     maxv = arr.max()
     thresh = thr * maxv
@@ -90,6 +90,16 @@ def best_circle_fit(image, thr:float=0.5):
     c, *_ = np.linalg.lstsq(A, b, rcond=None)
     cx, cy, c0 = c
     radius = np.sqrt(cx * cx + cy * cy + c0)
+    if show:
+        plt.figure()
+        plt.imshow(arr, origin='lower', cmap='gray')
+        theta = np.linspace(0, 2*np.pi, 200)
+        circle_x = cx + radius * np.cos(theta)
+        circle_y = cy + radius * np.sin(theta)
+        plt.plot(circle_x, circle_y, '--', c='C0')
+        plt.plot(cx, cy, '+', c='C0')
+        plt.xlim([cx-2.4*radius,cx+2.4*radius])
+        plt.ylim([cy-2.4*radius,cy+2.4*radius])
     return cx, cy, radius
 
 
@@ -99,29 +109,33 @@ def normalize_frame(frame):
     return arr / mean_val
 
 
-def circularize_ellipse(cam, freq:float, Nits:int=1, dV:float=0.1, dphi:float=1, gain:float=0.5, Nframes:int=2, debug:bool=False):
+def circularize_ellipse(cam, freq:float, Nits:int=1, dV:float=0.2, dphi:float=2.0, 
+                        gain:float=1.0, Nframes:int=2, debug:bool=False,
+                        amp1=1.0, amp2=1.0, phi2=90):
 
     # Set input waves to correct frequency and align phase
-    amp1 = wg.get_ampl(ch=1)
-    amp2 = wg.get_ampl(ch=2)
-    phi2 = wg.get_phase(ch=2)
     wg.set_wave(ch=1,ampl=amp1,offs=0,freq=freq,wave_form='SIN')
+    time.sleep(0.4)
     wg.set_wave(ch=2,ampl=amp2,offs=0,freq=freq,wave_form='SIN')    
+    time.sleep(0.4)
+    wg.set_phase(ch=2,phase=phi2)
+    time.sleep(0.4)
     wg.phase_align()
 
     # Acquire reference frame
     print('Acquiring reference frame ...')
-    start_frame = normalize_frame(cam.acquire_frames(Nframes))
+    start_frame = cam.acquire_frames(Nframes)
     cx,cy,radius = best_circle_fit(start_frame)
 
     ymin = max(int(np.floor(cy - 1.4*radius)), 0)
     ymax = min(int(np.ceil(cy + 1.4*radius)), start_frame.shape[0])
     xmin = max(int(np.floor(cx - 1.4*radius)), 0)
     xmax = min(int(np.ceil(cx + 1.4*radius)), start_frame.shape[1])
-    arr_crop = start_frame[ymin:ymax, xmin:xmax]
+    arr_crop = normalize_frame(start_frame[ymin:ymax, xmin:xmax])
     if debug:
         show_frame(arr_crop,title='Cropped frame')
     
+    # Reference frame
     cx_local = cx - xmin
     cy_local = cy - ymin
     yy, xx = np.indices(arr_crop.shape)
@@ -144,11 +158,12 @@ def circularize_ellipse(cam, freq:float, Nits:int=1, dV:float=0.1, dphi:float=1,
     # Change wave 1 amplitude
     print('Computing interation matrix: ch1 amplitude ...')
     wg.set_wave(ch=1,ampl=amp1+dV,offs=0,freq=freq,wave_form='SIN')
-    push_frame = normalize_frame(cam.acquire_frames(Nframes))[ymin:ymax, xmin:xmax]
+    push_frame = normalize_frame(cam.acquire_frames(Nframes)[ymin:ymax, xmin:xmax])
     wg.set_wave(ch=1,ampl=amp1-dV,offs=0,freq=freq,wave_form='SIN')
-    pull_frame = normalize_frame(cam.acquire_frames(Nframes))[ymin:ymax, xmin:xmax]
+    pull_frame = normalize_frame(cam.acquire_frames(Nframes)[ymin:ymax, xmin:xmax])
     wg.set_wave(ch=1,ampl=amp1,offs=0,freq=freq,wave_form='SIN')
     dframe = push_frame - pull_frame
+    dframe -= np.mean(dframe)
     IM[:,0] = dframe.flatten()/(2*dV)
     if debug:
         show_frame(dframe/(2*dV), title='Wave 1 Amplitude Change')
@@ -156,11 +171,12 @@ def circularize_ellipse(cam, freq:float, Nits:int=1, dV:float=0.1, dphi:float=1,
     # Change wave 2 amplitude    
     print('Computing interation matrix: ch2 amplitude ...')
     wg.set_wave(ch=2,ampl=amp2+dV,offs=0,freq=freq,wave_form='SIN')
-    push_frame = normalize_frame(cam.acquire_frames(Nframes))[ymin:ymax, xmin:xmax]
+    push_frame = normalize_frame(cam.acquire_frames(Nframes)[ymin:ymax, xmin:xmax])
     wg.set_wave(ch=2,ampl=amp2-dV,offs=0,freq=freq,wave_form='SIN')
-    pull_frame = normalize_frame(cam.acquire_frames(Nframes))[ymin:ymax, xmin:xmax]
+    pull_frame = normalize_frame(cam.acquire_frames(Nframes)[ymin:ymax, xmin:xmax])
     wg.set_wave(ch=2,ampl=amp2,offs=0,freq=freq,wave_form='SIN')
     dframe = push_frame - pull_frame
+    dframe -= np.mean(dframe)
     IM[:,1] = dframe.flatten()/(2*dV)
     if debug:
         show_frame(dframe/(2*dV), title='Wave 2 Amplitude Change')
@@ -169,13 +185,14 @@ def circularize_ellipse(cam, freq:float, Nits:int=1, dV:float=0.1, dphi:float=1,
     print('Computing interation matrix: ch2 phase ...')
     wg.set_phase(ch=2,phase=phi2+dphi)
     wg.phase_align()
-    push_frame = normalize_frame(cam.acquire_frames(Nframes))[ymin:ymax, xmin:xmax]
+    push_frame = normalize_frame(cam.acquire_frames(Nframes)[ymin:ymax, xmin:xmax])
     wg.set_phase(ch=2,phase=phi2-dphi)
     wg.phase_align()
-    pull_frame = normalize_frame(cam.acquire_frames(Nframes))[ymin:ymax, xmin:xmax]
+    pull_frame = normalize_frame(cam.acquire_frames(Nframes)[ymin:ymax, xmin:xmax])
     wg.set_phase(ch=2,phase=phi2)
     wg.phase_align()
     dframe = push_frame - pull_frame
+    dframe -= np.mean(dframe)
     IM[:,2] = dframe.flatten()/(2*dphi)
     if debug:
         show_frame(dframe/(2*dphi), title='Wave 2 Phase Change')
@@ -187,9 +204,13 @@ def circularize_ellipse(cam, freq:float, Nits:int=1, dV:float=0.1, dphi:float=1,
     for i in range(Nits):
         arr_crop = end_crop.copy()
         error = ref_frame.flatten() - arr_crop.flatten()
+        # error -= np.mean(error)
         if debug:
             show_frame(error.reshape(arr_crop.shape), title='Detected difference from circle')
         dcmd = Rec @ error
+        dcmd[:2] = np.clip(dcmd[:2], -dV, dV)
+        dcmd[2] = np.clip(dcmd[2], -dphi, dphi)
+        dcmd[:2] /= 2 # split correction between two channels
         dcmd *= gain
         new_circle = arr_crop.flatten() + IM @ dcmd
         if debug:
@@ -203,16 +224,18 @@ def circularize_ellipse(cam, freq:float, Nits:int=1, dV:float=0.1, dphi:float=1,
         wg.set_phase(ch=2,phase=phi2)
         wg.phase_align()
 
-        end_frame = normalize_frame(cam.acquire_frames(Nframes))
-        end_crop = end_frame[ymin:ymax, xmin:xmax]
+        end_frame = cam.acquire_frames(Nframes)
+        end_crop = normalize_frame(end_frame[ymin:ymax, xmin:xmax])
 
         if debug:
             show_frame(new_circle.reshape(arr_crop.shape)-end_crop, title='Predicted - obtained circle after correction')
         end_err_rms = np.sqrt(np.mean((end_crop.flatten() - ref_frame.flatten())**2))
-        print(f'\tIteration {i:1.0f}: error RMS = {end_err_rms:.4g}, ch1+={dcmd[0]:1.2f}V, ch2+={dcmd[1]:1.2f}V, {dcmd[2]:1.2f}°')
+        print(f'\tIteration {i+1:1.0f}: error RMS = {end_err_rms:.4g}, ch1+={dcmd[0]*1e+3:1.0f}mV, ch2+={dcmd[1]*1e+3:1.0f}mV, {dcmd[2]:1.3f}°')
+        print(f'\t\tParameters: ch1={amp1*1e+3:1.0f}mV, ch2={amp2*1e+3:1.0f}mV, phi2={phi2:1.3f}°\n')
 
     if debug:
         show_frame(end_crop, title='Final frame after correction')
         show_frame(end_crop - arr_crop, title='Difference between final and reference frame')
+    
 
-    return start_frame, end_frame, dcmd
+    return start_frame, end_frame, amp1, amp2, phi2
